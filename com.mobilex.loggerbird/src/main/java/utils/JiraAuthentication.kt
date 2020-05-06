166c270ede63e169191b599af512ee0ddf856485
berkavcioglu <berk.avcioglu@mobilex.com.tr>
2020-05-06 19:33:35 +0300

    Merge branches 'development' and 'feature/temp_service' of https://git.mobilex.com.tr/fireapp/loggerbird into feature/temp_service
     Conflicts:
    	app/src/main/java/deneme/example/filedeneme/Main3Activity.kt
    	app/src/main/java/deneme/example/filedeneme/MainActivity.kt
    	app/src/main/java/deneme/example/filedeneme/RecyclerViewAdapter.kt
    	app/src/main/res/layout/recycler_view_item.xml
    	com.mobilex.loggerbird/build.gradle
    	com.mobilex.loggerbird/src/main/AndroidManifest.xml
    	com.mobilex.loggerbird/src/main/java/fragments/FragmentLoggerBird.kt
    	com.mobilex.loggerbird/src/main/java/observers/LogActivityLifeCycleObserver.kt
    	com.mobilex.loggerbird/src/main/java/paint/PaintActivity.kt
    	com.mobilex.loggerbird/src/main/java/paint/PaintFabAnimation.kt
    	com.mobilex.loggerbird/src/main/java/paint/PaintView.kt
    	com.mobilex.loggerbird/src/main/res/layout/activity_paint.xml
    	com.mobilex.loggerbird/src/main/res/layout/activity_paint_save_snackbar.xml
    	com.mobilex.loggerbird/src/main/res/layout/fragment_logger_bird.xml
    	com.mobilex.loggerbird/src/main/res/values/colors.xml

--- com.mobilex.loggerbird/src/main/java/utils/JiraAuthentication.kt
+++ com.mobilex.loggerbird/src/main/java/utils/JiraAuthentication.kt
@@ -5,15 +5,25 @@ import com.atlassian.jira.rest.client.api.JiraRestClientFactory
 import com.atlassian.jira.rest.client.api.domain.Issue
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory
 import io.atlassian.util.concurrent.Promise
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import java.net.URI
 
 class JiraAuthentication {
+    private val coroutineCallJira:CoroutineScope = CoroutineScope(Dispatchers.IO)
     internal fun callJiraIssue(){
-        val factory :JiraRestClientFactory = AsynchronousJiraRestClientFactory()
-        val jiraServerUri:URI= URI("https://mobilex.atlassian.net/secure/Dashboard.jspa")
-        val restClient:JiraRestClient = factory.createWithBasicHttpAuthentication(jiraServerUri,"berk.avcioglu@mobilex.com.tr","umbasta1")
-        val promise : Promise<Issue> = restClient.issueClient.getIssue("LBG-30")
-        val issue:Issue = promise.claim()
-        Log.d("issue",issue.description!!)
+        coroutineCallJira.async {
+            try {
+                val factory :JiraRestClientFactory = AsynchronousJiraRestClientFactory()
+                val jiraServerUri:URI= URI("https://mobilex.atlassian.net/secure/Dashboard.jspa")
+                val restClient:JiraRestClient = factory.createWithBasicHttpAuthentication(jiraServerUri,"berk.avcioglu@mobilex.com.tr","umbasta1")
+                val promise : Promise<Issue> = restClient.issueClient.getIssue("LBG-30")
+                val issue:Issue = promise.claim()
+                Log.d("issue",issue.description!!)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
